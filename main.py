7978cd6996ea54b8159e7b722af7b13e97b9f41f
Andrew Melnik <andrew.melnik.git@gmail.com>
2019-01-31 05:22:42 +0100

    trg_gen_type == 'generate_vae_training_data'

--- main.py
+++ main.py
@@ -2,16 +2,56 @@ import pickle, os, gzip
 import numpy as np
 from pyquaternion import Quaternion
 from time import gmtime, strftime
+import math
+import matplotlib
 
-def generate_cloud(size=11,dimension=1.0):
+matplotlib.use('TkAgg')
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+
+
+def generate_cloud_cube(size=16,dimension=1.0):
     # generates point cloud with center 0,0,0
     pts = []
     for i in np.linspace(-dimension, dimension, size):
         for j in np.linspace(-dimension, dimension, size):
             for k in np.linspace(-dimension, dimension, size):
                 if abs(i) == dimension or abs(j) == dimension or abs(k) == dimension:
-                    pts.append(np.array([i, j, k]))
-    return pts
+                    pts.append([i, j, k])
+    return np.array(pts, dtype='float32')
+
+
+def generate_cloud_sphere(size=16,dimension=1.0):
+    # samples = int(round(size * size * np.sqrt(size)))
+    samples = int(round(size * size * size))
+    pts = []
+    offset = 2./samples
+    increment = math.pi * (3. - math.sqrt(5.))
+    for i in range(samples):
+        y = ((i * offset) - 1) + (offset / 2)
+        r = math.sqrt(1 - pow(y, 2))
+        phi = ((i + 1.) % samples) * increment
+        x = math.cos(phi) * r
+        z = math.sin(phi) * r
+        pts.append([x * dimension, y * dimension, z * dimension])
+    return np.array(pts, dtype='float32')
+
+
+def generate_cloud_pen(size=16, dimension=1.0):
+    pts = []
+    pen_2r = 0.008
+    pen_len = 0.1
+    r = (pen_2r / pen_len * dimension) / 2
+    # samples_r = np.linspace(0, 2, int(round(size/2)))
+    samples_r = np.linspace(0, 2, 16+1)
+    x, y = r * np.cos(samples_r * np.pi)[:-1], r * np.sin(samples_r * np.pi)[:-1]
+    l = np.linspace(-dimension, dimension, size)
+    for i in range(len(x)):
+        for zz in l:
+            pts.append([x[i], y[i], zz])
+    pts.append([0.0, 0.0, -dimension - r])
+    pts.append([0.0, 0.0, dimension + r])
+    return np.array(pts, dtype='float32')
 
 
 def transform_cloud(point_cloud, quaternion, cube_position=[0.0, 0.0, 0.0]):
@@ -36,7 +76,7 @@ def normaliza(cube_points, voxel_range):
 def cloud2voxel(cloud, voxel_range, size=16, shift=[0, 0, 0]):
     # convert to voxel grid
     size05 = (size - 1) / 2
-    voxels = np.zeros((size, size, size), dtype=np.uint8)
+    voxels = np.zeros((size, size, size), dtype=bool)
     pos_center = (size05, size05, size05)
     for pt in cloud:
         i, j, k = np.round(pt * size05 / voxel_range + pos_center).astype('int')
@@ -47,36 +87,39 @@ def cloud2voxel(cloud, voxel_range, size=16, shift=[0, 0, 0]):
         if i < size and j < size and k < size and i >= 0 and j >= 0 and k >= 0:
             voxels[int(i)][int(j)][int(k)] = 1
         else:
-            print(i, j, k)
-            quit()
+            print('quit()  >>  ', i, j, k)
+            return None
+            # quit()
     return voxels
 
 
 if __name__ == "__main__":
-    trg_gen_type = 'move_q0'  # 'rotate_q111'
-    trg_normalize_cloud_size = False
-
-    voxelSpaceSize = 16
-    voxelRange = 2.0
-    dimension = 1.25
-    # rotation of the points initialized in the 3D-voxel-cube may bring some of these points (e.g. vertices) outside of the 3D-voxel-cube
-    # to ensure all points are in the 3D-voxel-cube after a rotation, either set trg_normalize_cloud_size = True or set the ratio dimension/dimension <= ~0.6
-    cloud = generate_cloud(size=voxelSpaceSize, dimension=dimension)
+    trg_gen_type = 'generate_vae_training_data'  # rotate_q111 generate_vae_training_data move_q0 load_quat_seq_ep.pgz
     seedid = 42
 
-    quaternions = []
-    cloudShapes = []
-    voxelShapes = []
     np.random.seed(seedid)
 
     if trg_gen_type == 'rotate_q111':
+        trg_normalize_cloud_size = True
         numberOfShapes = 36  # 10000
+
+        # rotation of the points initialized in the 3D-voxel-cube may bring some of these points (e.g. vertices) outside of the 3D-voxel-cube
+        # to ensure all points are in the 3D-voxel-cube after a rotation, either set trg_normalize_cloud_size = True or set the ratio dimension/dimension <= ~0.6
+        voxelSpaceSize = 16
+        voxelRange = 2.0
+        dimension = 1.0
+        cloud = generate_cloud_cube(size=voxelSpaceSize, dimension=dimension)
+
+        quaternions = []
+        cloudShapes = []
+        voxelShapes = []
+
         for i in range(numberOfShapes):
             quat = Quaternion.random()
 
             angles = np.linspace(0, 2, numberOfShapes)
             quat = Quaternion(axis=[1, 1, 1], angle=np.pi * angles[i])
-            # if i == 0: quat = Quaternion(axis=[1, 1, 1], angle=np.pi / 4)
+            if i == 0: quat = Quaternion(axis=[1, 1, 1], angle=np.pi / 4)
             # if i == 0: quat = Quaternion()  # first quat = 1 + 0i + 0j + 0k
 
             if i % 100 == 0: print(strftime("%H%M%S", gmtime()), '\t', i, '\t', quat)
@@ -92,7 +135,24 @@ if __name__ == "__main__":
             cloudShapes.append(cloud_transformed)
             voxelShapes.append(voxels)
 
+        with gzip.GzipFile('assets' + os.sep + 'voxel_shapes_len' + str(len(voxelShapes)) + '_' + strftime("%H%M%S", gmtime()) + '.pgz', 'w') as f:
+            # with open('assets' + os.sep + 'voxel_shapes_len' + str(len(voxelShapes)) + '_' + strftime("%H%M%S", gmtime()) + '.pkl', 'wb') as f:
+            pickle.dump({'quaternions': quaternions, 'cloudShapes': cloudShapes, 'voxelShapes': voxelShapes, 'voxelSpaceSize': voxelSpaceSize, 'voxelRange': voxelRange, 'dimension': dimension, 'cloud': cloud, 'seedid': seedid}, f)
+
     elif trg_gen_type == 'move_q0':
+        trg_normalize_cloud_size = False
+
+        # rotation of the points initialized in the 3D-voxel-cube may bring some of these points (e.g. vertices) outside of the 3D-voxel-cube
+        # to ensure all points are in the 3D-voxel-cube after a rotation, either set trg_normalize_cloud_size = True or set the ratio dimension/dimension <= ~0.6
+        voxelSpaceSize = 16
+        voxelRange = 2.0
+        dimension = 1.0
+        cloud = generate_cloud_cube(size=voxelSpaceSize, dimension=dimension)
+
+        quaternions = []
+        cloudShapes = []
+        voxelShapes = []
+
         quat = Quaternion(axis=[1, 1, 1], angle=0)
 
         a = [range(-3, 4), range(-3, 4), range(-3, 4)]
@@ -108,8 +168,138 @@ if __name__ == "__main__":
             cloudShapes.append(cloud_transformed)
             voxelShapes.append(voxels)
 
-    with gzip.GzipFile('assets' + os.sep + 'voxel_shapes_len' + str(len(voxelShapes)) + '_' + strftime("%H%M%S", gmtime()) + '.pgz', 'w') as f:
-    # with open('assets' + os.sep + 'voxel_shapes_len' + str(len(voxelShapes)) + '_' + strftime("%H%M%S", gmtime()) + '.pkl', 'wb') as f:
-        pickle.dump({'quaternions': quaternions, 'cloudShapes': cloudShapes, 'voxelShapes': voxelShapes, 'voxelSpaceSize': voxelSpaceSize, 'voxelRange': voxelRange, 'dimension': dimension, 'cloud': cloud, 'seedid': seedid}, f)
+        with gzip.GzipFile('assets' + os.sep + 'voxel_shapes_len' + str(len(voxelShapes)) + '_' + strftime("%H%M%S", gmtime()) + '.pgz', 'w') as f:
+            # with open('assets' + os.sep + 'voxel_shapes_len' + str(len(voxelShapes)) + '_' + strftime("%H%M%S", gmtime()) + '.pkl', 'wb') as f:
+            pickle.dump({'quaternions': quaternions, 'cloudShapes': cloudShapes, 'voxelShapes': voxelShapes, 'voxelSpaceSize': voxelSpaceSize, 'voxelRange': voxelRange, 'dimension': dimension, 'cloud': cloud, 'seedid': seedid}, f)
+
+    elif trg_gen_type == 'load_quat_seq_ep.pgz':
+        trg_normalize_cloud_size = False
+
+        # rotation of the points initialized in the 3D-voxel-cube may bring some of these points (e.g. vertices) outside of the 3D-voxel-cube
+        # to ensure all points are in the 3D-voxel-cube after a rotation, either set trg_normalize_cloud_size = True or set the ratio dimension/dimension <= ~0.6
+        voxelSpaceSize = 16
+        voxelRange = 2.0
+        dimension = 1.0
+        cloud = generate_cloud_cube(size=voxelSpaceSize, dimension=dimension)
+
+        quaternions = []
+        cloudShapes = []
+        voxelShapes = []
+
+        file = 'tmp/0118_101917/ep.pgz'
+        with gzip.open(file, 'r') as f:
+            data = pickle.load(f)
+        for k in data.keys():
+            print(k, type(data[k]))
+        for i in range(len(data['achieved_goals'])):
+            quat = Quaternion(data['achieved_goals'][i][0][3:])
+            if i % 100 == 0: print(strftime("%H%M%S", gmtime()), '\t', i, '\t', quat)
+            if trg_normalize_cloud_size:
+                cloud_transformed = normaliza(transform_cloud(cloud, quat), voxelRange)
+            else:
+                cloud_transformed = transform_cloud(cloud, quat)
+            voxels = cloud2voxel(cloud_transformed, voxelRange, size=voxelSpaceSize)
+            quaternions.append(quat)
+            cloudShapes.append(cloud_transformed)
+            voxelShapes.append(voxels)
+
+        with gzip.GzipFile('assets' + os.sep + 'voxel_shapes_len' + str(len(voxelShapes)) + '_' + strftime("%H%M%S", gmtime()) + '.pgz', 'w') as f:
+            # with open('assets' + os.sep + 'voxel_shapes_len' + str(len(voxelShapes)) + '_' + strftime("%H%M%S", gmtime()) + '.pkl', 'wb') as f:
+            pickle.dump({'quaternions': quaternions, 'cloudShapes': cloudShapes, 'voxelShapes': voxelShapes, 'voxelSpaceSize': voxelSpaceSize, 'voxelRange': voxelRange, 'dimension': dimension, 'cloud': cloud, 'seedid': seedid}, f)
+
+    elif trg_gen_type == 'generate_vae_training_data':
+        voxel_training_data = dict()
+        voxel_training_data['cube'] = {}
+        voxel_training_data['cube']['size'] = []
+        voxel_training_data['cube']['quat'] = []
+        # voxel_training_data['cube']['cloud'] = []
+        voxel_training_data['cube']['voxel'] = []
+        voxel_training_data['pen'] = {}
+        voxel_training_data['pen']['size'] = []
+        voxel_training_data['pen']['quat'] = []
+        # voxel_training_data['pen']['cloud'] = []
+        voxel_training_data['pen']['voxel'] = []
+        voxel_training_data['sphere'] = {}
+        voxel_training_data['sphere']['size'] = []
+        voxel_training_data['sphere']['quat'] = []
+        # voxel_training_data['sphere']['cloud'] = []
+        voxel_training_data['sphere']['voxel'] = []
+
+        # rotation of the points initialized in the 3D-voxel-cube may bring some of these points (e.g. vertices) outside of the 3D-voxel-cube
+        # to ensure all points are in the 3D-voxel-cube after a rotation, either set trg_normalize_cloud_size = True or set the ratio dimension/dimension <= ~0.6
+        voxelSpaceSize = 16
+        voxelRange = 2.0
+
+        range_quat_cube = 30  #* 1000
+        range_quat_pen = 10  #*1000
+
+        # voxel_training_data['pen']['voxel'][-1]
+        for shapeSize in [8, 9, 10, 11]:
+            dimension = voxelRange * shapeSize / voxelSpaceSize
+            cloud = generate_cloud_cube(size=voxelSpaceSize, dimension=dimension)
+            for i in range(range_quat_cube):
+                quat = Quaternion.random()
+                # if i == 0: quat = Quaternion(axis=[1, 1, 1], angle=np.pi / 4)
+                if i == 0: quat = Quaternion()  # first quat = 1 + 0i + 0j + 0k
+                if i % 100 == 0: print(strftime("%H%M%S", gmtime()), 'cube', '\t', 'shapeSize', shapeSize, '\t', 'i', i, '\t', 'quat', quat)
+                cloud_transformed = transform_cloud(cloud, quat)
+                voxels = cloud2voxel(cloud_transformed, voxelRange, size=voxelSpaceSize)
+                if voxels is not None:
+                    voxel_training_data['cube']['size'].append(shapeSize)
+                    voxel_training_data['cube']['quat'].append(quat)
+                    # voxel_training_data['cube']['cloud_transformed'].append(cloud_transformed)
+                    voxel_training_data['cube']['voxel'].append(voxels)
+        voxel_training_data['cube']['len'] = len(voxel_training_data['cube']['voxel'])
+        print('len', "['cube']['voxel']", len(voxel_training_data['cube']['voxel']))
+
+        for shapeSize in [8, 9, 10, 11, 12, 13, 14, 15, 16]:
+            dimension = voxelRange * shapeSize / voxelSpaceSize
+            cloud = generate_cloud_sphere(size=voxelSpaceSize, dimension=dimension)
+
+            quat = Quaternion()
+            cloud_transformed = transform_cloud(cloud, quat)
+            print(strftime("%H%M%S", gmtime()), 'sphere', '\t', 'shapeSize', shapeSize, '\t', 'i', i, '\t', 'quat', quat)
+
+            # cloud = np.array(cloud)
+            # fig = plt.figure()
+            # ax = fig.add_subplot(111, projection='3d')
+            # ax.set_xlim3d(-1.5, 1.5)
+            # ax.set_ylim3d(-1.5, 1.5)
+            # ax.set_zlim3d(-1.5, 1.5)
+            # ax.scatter(cloud[:, 0], cloud[:, 1], cloud[:, 2])
+            # plt.show()
+
+            voxels = cloud2voxel(cloud_transformed, voxelRange, size=voxelSpaceSize)
+
+            voxel_training_data['sphere']['size'].append(shapeSize)
+            voxel_training_data['sphere']['quat'].append(quat)
+            # voxel_training_data['sphere']['cloud_transformed'].append(cloud_transformed)
+            voxel_training_data['sphere']['voxel'].append(voxels)
+        voxel_training_data['sphere']['len'] = len(voxel_training_data['sphere']['voxel'])
+        print('len', "['sphere']['voxel']", len(voxel_training_data['sphere']['voxel']))
+
+        for shapeSize in [8, 9, 10, 11, 12, 13, 14, 15, 16]:
+            dimension = voxelRange * shapeSize / voxelSpaceSize
+            cloud = generate_cloud_pen(size=voxelSpaceSize, dimension=dimension)
+            for i in range(range_quat_pen):
+                quat = Quaternion.random()
+                # if i == 0: quat = Quaternion(axis=[1, 1, 1], angle=np.pi / 4)
+                if i == 0: quat = Quaternion()  # first quat = 1 + 0i + 0j + 0k
+                if i % 100 == 0: print(strftime("%H%M%S", gmtime()), 'pen', '\t', 'shapeSize', shapeSize, '\t', 'i', i, '\t', 'quat', quat)
+                cloud_transformed = transform_cloud(cloud, quat)
+                voxels = cloud2voxel(cloud_transformed, voxelRange, size=voxelSpaceSize)
+                if voxels is not None:
+                    voxel_training_data['pen']['size'].append(shapeSize)
+                    voxel_training_data['pen']['quat'].append(quat)
+                    # voxel_training_data['pen']['cloud_transformed'].append(cloud_transformed)
+                    voxel_training_data['pen']['voxel'].append(voxels)
+        voxel_training_data['pen']['len'] = len(voxel_training_data['pen']['voxel'])
+        print('len', "['pen']['voxel']", len(voxel_training_data['pen']['voxel']))
+
+        with gzip.GzipFile('assets' + os.sep + 'voxel_cube_sphere_pen' + '_' + strftime("%H%M%S", gmtime()) + '.pgz', 'w') as f:
+            pickle.dump({'voxel_training_data': voxel_training_data, 'voxelSpaceSize': voxelSpaceSize, 'seedid': seedid}, f)
+
+    # TODO run on feb
+    # TODO write 16/64 shift function
 
     print('DONE')
--- plot_voxel_view.py
+++ plot_voxel_view.py
@@ -1,4 +1,5 @@
-import pickle, os
+import pickle, os, subprocess
+from time import gmtime, strftime
 from PIL import Image
 import numpy as np
 import matplotlib
@@ -6,12 +7,14 @@ matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import  # This import registers the 3D projection, but is otherwise unused.
 
-file = 'assets' + os.sep + 'voxel_shapes_len343_ss16_cs10_q0_shift3px.pgz'
+file = 'assets' + os.sep + 'voxel_shapes_len200_0118_101917_ep.pgz'
+# file = 'assets' + os.sep + 'voxel_shapes_len343_ss16_cs10_q0_shift3px.pgz'
 # file = 'assets' + os.sep + 'voxel_shapes_len36_095525.pkl'
 trg_plot_type = 'quat_sequence'  # ['quat_first_rotate', 'quat_sequence']
 
 trg_im_save = True
-trg_gif = True
+trg_gif = False
+trg_mp4 = True
 
 if file.split('.')[-1] == 'pkl':
     with open(file, 'rb') as f:
@@ -46,28 +49,30 @@ if trg_plot_type == 'first_rotate':
 elif trg_plot_type == 'quat_sequence':
     fig = plt.figure(figsize=(8, 8), dpi=100)
     ax = fig.gca(projection='3d')
+    im_folder = 'png_' + strftime("%H%M%S", gmtime())
     for i in range(len(voxel_shapes['quaternions'])):
         quat = voxel_shapes['quaternions'][i]
         voxels = voxel_shapes['voxelShapes'][i] > 0
-        print('quat = ', quat, '\n')
+        print('i = ', i, '\t\tquat = ', quat, '\n')
         ax.cla()
         ax.voxels(voxels, facecolors=[1, 0, 0, 0.3], edgecolor=[0, 0, 0, 0.5])
-        # angles = np.linspace(0, 2, len(voxel_shapes['quaternions']))
-        # plt.title('quat = Quaternion(axis=[1, 1, 1], angle=np.pi * ' + str(round(angles[i], 2)) + '\n' + str(quat))
+        plt.title('i=' + str(i) + '    quat=' + str(quat))
         # ax.view_init(0, 181)
         plt.draw()
         plt.pause(.001)
         if trg_im_save:
             im = Image.fromarray(np.array(np.array(fig.canvas.renderer._renderer), dtype=np.uint8))
-            if not os.path.exists('png'):
-                os.makedirs('png')
-            im.save('png' + os.sep + 'im' + str(i).zfill(4) + '.png')
+            if not os.path.exists(im_folder):
+                os.makedirs(im_folder)
+            im.save(im_folder + os.sep + 'im' + str(i).zfill(4) + '.png')
     if trg_gif:
         # cmd = "cd png; ffmpeg -framerate 10 -pattern_type glob -i '*0.png' -vf format=rgb24 _output.gif"
         # cmd = "cd png; convert -loop 0 -delay 10 '*0.png' _output2.gif"
-        cmd = "cd png; convert -delay 20 -loop 0 -dither None -colors 32 '*0.png' -fuzz '40%' -layers OptimizeFrame '_output.gif'"
-        import subprocess
+        cmd = "cd " + im_folder + "; convert -delay 20 -loop 0 -dither None -colors 32 '*0.png' -fuzz '40%' -layers OptimizeFrame '_output.gif'"
+        subprocess.call(cmd, shell=True)
+    if trg_mp4:
+        cmd = "cd " + im_folder + "; ffmpeg -framerate 15 -pattern_type glob -i '*.png' -c:v libx264 -pix_fmt yuv420p -g 1 _output.mp4"
         subprocess.call(cmd, shell=True)
 
-plt.pause(60)
+plt.pause(30)
 print('EXIT')
